87f5aaf05fcbd9f4c1c184c7c51cee960742437c
Redthorn <redthorn57@yahoo.com>
2013-12-20 15:06:02 -0500

    delete unneeded modules and ties

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ var routes = require('./routes');
 var http = require('http');
 var path = require('path');
 var fs = require('fs');
-var twitter = require('ntwitter');
-var sentiment = require('sentiment');
 
 var app = express();
 
@@ -36,23 +34,6 @@ app.get('/results', function (req, res){res.render('results.html');});
 app.get('/hashlist', function (req, res){res.render('hashlist.html');});
 app.get('/selection', function (req, res){res.render('selection.html');});
 
-//authenticate with twitter dev account
-var twit = new twitter({
-  consumer_key: 'zjVQiFUnRX5ikVCLVgIw',
-  consumer_secret: 'pNllpbhujW2inNcNCQ8ktIIQGRaL70dzLBm9tXNLHLc',
-  access_token_key: '43466231-KhsLhbx53cgremwCTYeY80na6FfCoUwiPlNO5ylaL',
-  access_token_secret: 'tF7bYNLjDLcOZRmuhUSK0lucn4ZkF1jGZgbRs0DGPQOMb'
-});
- twit
-   .verifyCredentials(function (err, data) {
-    console.log("Verifying Credentials...");
-    if(err)
-      console.log("Verification Failed : " + err)
-      else
-          //console.log(data);
-        console.log("Verification Success");
-})
-
 var hashlist = new Array(25);
 var hashfreq = new Array(25);
 for(var i=0; i<25; i++)
@@ -71,66 +52,6 @@ app.post('/form', function (req, res)
   var lat = (req.body.lat);
   var lon = (req.body.lon);
   //console.log(hash1,hash2,startDate,endDate,radius,lat,lon);
-
-  //sentiment evaluation and construct soundcloud api implementation
-  var score1, score2, Total;
-  sentiment(hash1, function (err, result1){
-    score1 = (result1.score);
-  });
-  sentiment(hash2, function (err, result2){
-    score2 = (result2.score);
-  });
-  Total = score1 + score2;
-  var string = '<strong>Sentiment Score:</strong> ' + score1 + ' + ' + score2 + ' = ' + Total + '<p>';
-  if(Total <= 1 && Total >= -1)
-  {
-    string += 'Neutral Sentiment: ' + '<p> (-4)  (-3 - -2) <b>(-1 - +1)</b> (+2 - +3) (4+)';
-  }
-  else if(Total == -2 || Total == -3)
-  {
-    string += 'Negative Sentiment: ' + '<p> (-4)  <b>(-3 - -2)</b> (-1 - +1) (+2 - +3) (4+)';
-  }
-  else if(Total == 2 || Total == 3)
-  {
-    string += 'Positive Sentiment: ' + '<p> (-4)  (-3 - -2) (-1 - +1) <b>(+2 - +3)</b> (4+)';
-  }
-  else if (Total >= 4)
-  {
-    string += 'Very Positive Sentiment: ' + '<p> (-4)  (-3 - -2) (-1 - +1) (+2 - +3) <b>(4+)</b>';
-  }
-  else if (Total <= -4)
-  {
-    string += 'Very Negative Sentiment: ' + '<p> <b>(-4)</b>  (-3 - -2) (-1 - +1) (+2 - +3) (4+)';
-  }
-
-  string += '</p><p><iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/'
-
-  if(Total <= 1 && Total >= -1)
-  {
-    string += '85910201';
-  }
-  else if(Total == -2 || Total == -3)
-  {
-    string += '60360398';
-  }
-  else if(Total == 2 || Total == 3)
-  {
-    string += '65625445';
-  }
-  else if (Total >= 4)
-  {
-    string += '106687285';
-  }
-  else if (Total <= -4)
-  {
-    string += '122952876';
-  }
-
-  string += '&amp;color=00FF00&amp;auto_play=false&amp;show_artwork=true"></iframe></p>';
-  fs.writeFile('views/selection.html', string, function (err) {
-      if (err) throw err;
-      console.log('Sentiment saved!');
-  });
 
 	//search and insert input1 to arrays
 	for(var i=0; i < 25; i++)
