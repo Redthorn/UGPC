612309ccdd998f809dc31b14037fa02d5438138f
Redthorn <redthorn57@yahoo.com>
2013-12-21 15:40:57 -0500

    Template and CSS

--- routes/index.js
+++ routes/index.js
@@ -7,10 +7,6 @@ exports.index = function(req, res){
   res.render('index', { title: 'Form' });
 };
 
-exports.results = function(req, res){
-	res.render('results', {title: 'Results'});
-};
-
 exports.hashlist = function(req, res){
   res.render('hashlist', {title: 'Hashlist'});
 };
